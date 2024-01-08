67144cc3e0b3abe77ff7ef8bde284907ef43098e
jacniluk <jacniluk@wp.pl>
2024-01-08 11:53:30 +0100

    Factors Settings scriptable + initial data

---
+++ Travelers/Assets/Game/Scripts/Scriptables/FactorsSettings.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FactorsSettings", menuName = "Scriptables/Factors Settings")]
+public class FactorsSettings : ScriptableObject
+{
+    [Header("Factors")]
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float minTurnSpeed;
+    [SerializeField] private float maxTurnSpeed;
+}
